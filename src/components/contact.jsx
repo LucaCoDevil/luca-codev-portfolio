3e1fc02e294d8c1aaec5501d7531a677f136f03d
LucaCoDevil <lucacodevilla123@gmail.com>
2021-03-30 09:36:23 +0200

    add emailjs

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,26 +1,45 @@
 import React from "react";
+import emailjs from 'emailjs-com';
 
 const contact = () => {
+
+  const {REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, REACT_APP_USER_ID} = process.env;
+
+  function sendEmail(e) {
+    e.preventDefault();
+  
+    emailjs.sendForm(REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, e.target, REACT_APP_USER_ID)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  e.target.reset();
+  }
+
   return (
     <div id="contact-container">
       <h2 style={{ color: "#dbdbdb", borderColor: "#dbdbdb" }}>Contact</h2>
       <div className="form-align">
-        <form action="" className="form-controller">
+        <form onSubmit={sendEmail} className="form-controller">
           <input
             type="text"
             className="input-controller"
-            placeholder="First name"
+            placeholder="Name"
+            name='user_name'
           />
           <input
-            type="text"
+            type="email"
             className="input-controller"
-            placeholder="Last Name"
+            placeholder="Email"
+            name='user_email'
           />
           <textarea
             cols="50"
             className="input-controller"
             placeholder="Message"
             rows="8"
+            name='message'
           ></textarea>
           <button type="submit" className="submit-btn">
             Submit
