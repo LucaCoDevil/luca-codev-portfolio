892b02d88360bdb8897909558cdaf48129ed1760
LucaCoDevil <lucacodevilla123@gmail.com>
2021-05-31 16:23:58 +0200

    fix bug

--- src/components/sub-components/card.jsx
+++ src/components/sub-components/card.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-const card = (props) => {
+const Card = (props) => {
+  const [isTouch, setisTouch] = useState(false);
+
+  window.addEventListener("touchstart", () => {
+    setisTouch(true);
+  });
+
   const handleClick = () => {
     window.open(props.projectURL);
   };
 
   return (
-    <div className="col" onTouchStart={handleClick}>
+    <div className="col" onClick={isTouch ? handleClick : null}>
       <img className="card-img" src={props.imgURL} alt={props.alt} />
       <div className="card-middle">
         <p className="card-description">{props.description}</p>
@@ -23,4 +29,4 @@ const card = (props) => {
   );
 };
 
-export default card;
+export default Card;
