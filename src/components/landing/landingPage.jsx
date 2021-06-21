2f5c05aa59e3bde6ce9e3f85360ad4395417b326
LucaCoDevil <lucacodevilla123@gmail.com>
2021-06-21 11:58:32 +0200

    fix sub-search

--- src/components/landing/landingPage.jsx
+++ src/components/landing/landingPage.jsx
@@ -3,25 +3,21 @@ import "./landing.css";
 import Nav from "../navigation/navigation";
 import SliderButton from "../slideDrawer/slideDrawerButton";
 
-
 const LandingPage = (props) => {
-
-
   return (
     <div id="landing">
       <div className="clip01"></div>
 
       <div className="clip02"></div>
-      
+
       <div id="landing-container">
         <h1 className="main-header">Luca Codevilla</h1>
         <h4 className="main-description">Full-stack web developer</h4>
         {/* <button id="cta-btn">CTA</button> */}
       </div>
-      <Nav/>
+      <Nav />
       <SliderButton onClick={props.handleClick} />
     </div>
-     
   );
 };
 
